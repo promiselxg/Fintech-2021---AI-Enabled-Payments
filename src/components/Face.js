772bfea60f9caf38f3e918c1f6d44c1221029d79
Promiselxg <okeydeede@gmail.com>
2021-04-25 06:41:25 +0100

    user registration

--- src/components/Face.js
+++ src/components/Face.js
@@ -1,10 +1,15 @@
 import React from "react";
-
+import "../style/Register.css";
+import Pics from "../Images/selfie.svg";
+import "../style/Face.css";
 const Face = () => {
   return (
-    <div className="face">
-      <h1>face recognition</h1>
-    </div>
+    <>
+      <div className="face">
+        <h1>face recognition</h1>
+        <img src={Pics} alt="face recognition" />
+      </div>
+    </>
   );
 };
 
